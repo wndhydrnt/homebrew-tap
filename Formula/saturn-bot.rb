953da4c964048f747c6ed32734d8a51650bcfc2e
Markus Meyer <hydrantanderwand@gmail.com>
2024-12-01 15:57:10 +0100

    changes by saturn-bot (#7)
    Auto-merge by saturn-bot

--- Formula/saturn-bot.rb
+++ Formula/saturn-bot.rb
@@ -1,11 +1,11 @@
 class SaturnBot < Formula
   desc "Create, modify or delete files across many repositories in parallel."
   homepage "https://github.com/wndhydrnt/saturn-bot"
-  url "https://github.com/wndhydrnt/saturn-bot/archive/refs/tags/v0.15.1.tar.gz"
-  sha256 "7077e2d50ec8da5cd6610918eeb5f20e124908da0fb2cba44fc9c93126ac2669"
+  url "https://github.com/wndhydrnt/saturn-bot/archive/refs/tags/v0.16.0.tar.gz"
+  sha256 "352782caba32871b1a7494f78fd1271ca20f3296dfa49f7948dbad0a49e5daff"
   license "AGPL-3.0"
   head "https://github.com/wndhydrnt/saturn-bot.git", branch: "main"
-  version "0.15.1"
+  version "0.16.0"
 
   depends_on "go" => :build
 
