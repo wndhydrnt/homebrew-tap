5689d9a636de2b6f550fccd4377d8aba64a4d2f0
Markus Meyer <hydrantanderwand@gmail.com>
2025-06-20 05:23:14 +0000

    changes by saturn-bot

--- Formula/saturn-bot.rb
+++ Formula/saturn-bot.rb
@@ -1,11 +1,11 @@
 class SaturnBot < Formula
   desc "Create, modify or delete files across many repositories in parallel."
   homepage "https://github.com/wndhydrnt/saturn-bot"
-  url "https://github.com/wndhydrnt/saturn-bot/archive/refs/tags/v0.27.0.tar.gz"
-  sha256 "f3c3afa5aa35b5eff45893f8a97d0f091634cb30e89f7fbcfbecd3cb0f1dc276"
+  url "https://github.com/wndhydrnt/saturn-bot/archive/refs/tags/v0.27.1.tar.gz"
+  sha256 "8a7769192ec8b6fcf342303c16f4de401226eda30e42942ce37154c3e1286b3d"
   license "AGPL-3.0"
   head "https://github.com/wndhydrnt/saturn-bot.git", branch: "main"
-  version "0.27.0"
+  version "0.27.1"
 
   depends_on "go" => :build
 
